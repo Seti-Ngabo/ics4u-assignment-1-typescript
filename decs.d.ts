09318fdcedd3fee59be7d8f3a7458dddfb18274e
Seti Ngabo <seti.ngabo@mths.ca>
2022-09-22 11:24:17 +0000

    commit message

---
+++ decs.d.ts
@@ -0,0 +1,1 @@
+declare module "random" // e.g declare module 'random'
---
+++ src/index.ts
@@ -0,0 +1,42 @@
+/**
+ * This program is about Rock-Paper-Scissors game
+ *     with a computer opponent.
+ *
+ * By:      Seti Ngabo
+ * Version: 1.0
+ * Since:   2022-09-20
+ */
+
+import promptSync from 'prompt-sync'
+
+// The program will show the user if he won or lost
+const prompt = promptSync()
+const rock = 1
+const paper = 2
+const scissor = 3
+const lower = 1
+const higher = 3
+
+const randomNumber = Math.floor(Math.random() * higher + lower)
+// input
+const userString = prompt('Choose one of the following(rock, paper, or scissors): ')
+const user = parseFloat(userString)
+// process & output
+if (user === randomNumber) {
+  console.log('You tied!')
+} else if (user === rock && randomNumber === paper) {
+  console.log('You lose, try again.')
+} else if (user === rock && randomNumber === scissor) {
+  console.log('You won!')
+} else if (user === paper && randomNumber === rock) {
+  console.log('You won!')
+} else if (user === paper && randomNumber === scissor) {
+  console.log('You lose, try again.')
+} else if (user === scissor && randomNumber === rock) {
+  console.log('You lose, try again.')
+} else if (user === scissor && randomNumber === paper) {
+  console.log('You won!')
+} else {  console.log('\nWrong input try again.')
+}
+
+console.log('\nDone.')
